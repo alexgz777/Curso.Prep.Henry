7238af2b8efcf62353afa3ebbf75aa4d0f281c51
alexgz777 <alexgz777@gmail.com>
2021-06-24 17:07:21 -0300

    agregando JSII.test.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -145,7 +145,7 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
   if(numero < 2) return false;
   if(numero === 2) return true;
-  for(var i = 2; i < numero; i++) {
+  for(let i = 2; i < numero; i++) {
     if(numero % i === 0) {
       return false;
     }
